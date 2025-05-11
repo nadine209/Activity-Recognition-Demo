a715b141d6810ad9693563d649d9ec8683dc3b14
nadine209 <165024506+nadine209@users.noreply.github.com>
2025-05-11 15:16:48 +0200

    Update test_temporal.py

--- test_temporal.py
+++ test_temporal.py
@@ -33,10 +33,10 @@ def test_during():
     assert allen.during() is True
 
 def test_finishes():
-    event_A = (100, 120)
-    event_B = (90, 120)  # Changement : event_B commence avant event_A
+    event_A = (100, 120)  # event_A commence à 100 et finit à 120
+    event_B = (120, 130)  # event_B commence exactement à la fin de event_A
     allen = AllenTemporalLogic(event_A, event_B)
-    assert allen.finishes() is False  # On s'attend à un False ici
+    assert allen.finishes() is True  # Maintenant, A finit exactement au début de B
 
 
 def test_equals():
