5f6e5b55fbcd8c2cb5157eab3e05e192fa67ee15
nadine209 <165024506+nadine209@users.noreply.github.com>
2025-05-11 13:51:35 +0200

    Update and rename iot_ml_pipeline to test_temporal.py

---
+++ test_temporal.py
@@ -0,0 +1,49 @@
+# test_temporal.py
+import pytest
+from temporal_rules_advanced import AllenTemporalLogic
+
+def test_before():
+    event_A = (100, 120)
+    event_B = (121, 150)
+    allen = AllenTemporalLogic(event_A, event_B)
+    assert allen.before() is True
+
+def test_meets():
+    event_A = (100, 120)
+    event_B = (120, 150)
+    allen = AllenTemporalLogic(event_A, event_B)
+    assert allen.meets() is True
+
+def test_overlaps():
+    event_A = (100, 130)
+    event_B = (120, 150)
+    allen = AllenTemporalLogic(event_A, event_B)
+    assert allen.overlaps() is True
+
+def test_starts():
+    event_A = (100, 120)
+    event_B = (100, 150)
+    allen = AllenTemporalLogic(event_A, event_B)
+    assert allen.starts() is True
+
+def test_during():
+    event_A = (110, 120)
+    event_B = (100, 150)
+    allen = AllenTemporalLogic(event_A, event_B)
+    assert allen.during() is True
+
+def test_finishes():
+    event_A = (100, 120)
+    event_B = (90, 120)
+    allen = AllenTemporalLogic(event_A, event_B)
+    assert allen.finishes() is True
+
+def test_equals():
+    event_A = (100, 120)
+    event_B = (100, 120)
+    allen = AllenTemporalLogic(event_A, event_B)
+    assert allen.equals() is True
+
+if __name__ == "__main__":
+    pytest.main()
+
